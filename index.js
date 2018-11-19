e9e8a3aa294f5306ea16e89b55e831c089b5ef40
Xignotic <Xignotic84@gmail.com>
2018-11-19 10:14:01 +0800

    Jonny is cool (not rlly)

--- index.js
+++ index.js
@@ -2,31 +2,51 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const { TOKEN, PREFIX, OWNER } = require('./config.js');
 
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity(`${client.users.size} idiots`, { type: "WATCHING" });
+});
+
+client.on('error', () => {
+    console.log(`${error}`)
+});
+
 client.on('message', message => {
-
     let args = message.content.split(' ').slice(1);
-
     if (!message.content.startsWith(PREFIX)) return;
     if (message.channel.type === 'dm') return;
     if (message.author.bot) return;
 
+    if (message.content.startsWith(PREFIX + "ping")) {
+        return message.channel.send(client.ping);
+    }
+    if (message.content.startsWith(PREFIX + "jonny")) {
+        return message.channel.send("What did you say you cunt?");
+    }
 
-    if (message.content.startsWith(PREFIX + "Jonny")) {
-       return message.channel.send("What you cunt?");
+    if (message.content.toLowerCase().startsWith(PREFIX + "help")) {
+        message.channel.send("")
     }
-    if(message.conent.startWish(PREFIX + "serverinfo")) {
+
+    if (message.content.toLowerCase().startsWith(PREFIX + "serverinfo")) {
         const embed = new Discord.MessageEmbed()
-              .setTitle('Server Info')
-              .setAuthor(message.author.tag, message.author.displayAvatarURL())
-              .setColor(0xFF0000)
-              .setTimestamp()
-          message.channel.send(embed);
+            .setTitle('Server Info')
+            .setAuthor(message.author.tag, message.author.displayAvatarURL(), "Htt")
+
+            .setColor(0xFF0000)
+            .setTimestamp()
+        message.channel.send(embed);
     }
-    if(message.content.startsWith(PREFIX + "eval")) {
-        if(!message.author.id === OWNER) {
+
+    if (message.content.toLowerCase().startsWith(PREFIX + "eval")) {
+        if (!message.author.id === OWNER) {
             return message.channel.send("You don't own me you neckass")
         }
-        
+        message.channel.send("Yes")
+    }
+
+    if (message.content.toLowerCase().startsWith(PREFIX + "xignotic")) {
+        message.channel.send("Fuck you Xignotic")
     }
 })
 
