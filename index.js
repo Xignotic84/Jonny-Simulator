76d03526e4204738ce718705130a3dfa4227964e
Xignotic <Xignotic84@gmail.com>
2018-11-19 10:30:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { TOKEN, PREFIX, OWNER } = require('./config.js');
+const responses = [
+    `Fuck off ${message.author}`,
+    `Why do you have to bully me ${message.autor}`,
+    `Ken is gay`,
+    `Who wants to be on my headpat list`,
+    `I hate children`,
+    `No, neckass.`,
+    `stop with the fucking jonny jonny yes papas smh`,
+    `I have no socks on`,
+    `jonny not johny`,
+    `Fuck of Xig`
+]
+
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -17,9 +30,10 @@ client.on('message', message => {
     if (message.channel.type === 'dm') return;
     if (message.author.bot) return;
 
-    if (message.content.startsWith(PREFIX + "ping")) {
-        return message.channel.send(client.ping);
-    }
+    if (message.conente.startswit)
+        if (message.content.startsWith(PREFIX + "ping")) {
+            return message.channel.send(client.ping);
+        }
     if (message.content.startsWith(PREFIX + "jonny")) {
         return message.channel.send("What did you say you cunt?");
     }
@@ -31,7 +45,7 @@ client.on('message', message => {
     if (message.content.toLowerCase().startsWith(PREFIX + "serverinfo")) {
         const embed = new Discord.MessageEmbed()
             .setTitle('Server Info')
-            .setAuthor(message.author.tag, message.author.displayAvatarURL(), "Htt")
+            .setAuthor(message.author.tag, message.author.displayAvatarURL(), "htts://discordbots.org/bot/513875565447741440")
 
             .setColor(0xFF0000)
             .setTimestamp()
